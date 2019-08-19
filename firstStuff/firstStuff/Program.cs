52262cacd4de6ab3564b1e57d746066bc4c55668
Eric Naegle <mr.naegle@gmail.com>
2019-08-18 18:17:17 -0600

    halp I'm trying to commit

---
+++ firstStuff/firstStuff/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace firstStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second try!");
+            Console.ReadLine();
+        }
+    }
+}
